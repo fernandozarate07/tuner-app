931fdb6522b37258ed1ce25281e9da57385c7f34
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2024-12-16 15:39:51 -0300

    Add colors to the indicator line

--- src/components/Indicator/Indicator.jsx
+++ src/components/Indicator/Indicator.jsx
@@ -8,28 +8,40 @@ const Indicator = ({ frequency, note, selectedInstrument, instrumentNotes }) => 
     return 100 - distance * 15;
   };
 
-  const getLineColor = (index, selectedNoteFreq) => {
+  const getLineColor = (index) => {
     const activeIndex = 6;
-    const lineFreq = selectedNoteFreq + (index - activeIndex) * 5;
-    if (Math.abs(frequency - lineFreq) <= 2) {
-      return styles.activeLine;
+    const selectedNote = instrumentNotes.find((n) => n.note === note);
+    const lineFreq = selectedNote ? selectedNote.freq + (index - activeIndex) * 5 : 0;
+
+    // Usar una pequeña tolerancia para detectar si la frecuencia está cerca
+    const tolerance = 2; // 2 Hz de tolerancia
+
+    if (Math.abs(frequency - lineFreq) <= tolerance) {
+      // Aquí se cambia el color dependiendo de la cercanía
+      if (index < 3) {
+        return styles.redLine; // Primera sección roja
+      } else if (index < 6) {
+        return styles.yellowLine; // Segunda sección amarilla
+      } else if (index === activeIndex) {
+        return styles.greenLine; // Línea central verde
+      } else if (index < 10) {
+        return styles.yellowLine; // Cuarta sección amarilla
+      } else {
+        return styles.redLine; // Última sección roja
+      }
     }
-    return styles.inactiveLine;
+    return styles.inactiveLine; // Línea inactiva (negra por defecto)
   };
 
   return (
     <div className={styles.indicator}>
       <div className={styles.indicator__screen}>
         {[...Array(13)].map((_, index) => {
-          const selectedNote = instrumentNotes.find((n) => n.note === note);
-          const lineColor = selectedNote ? getLineColor(index, selectedNote.freq) : styles.inactiveLine;
+          const lineColor = getLineColor(index);
 
           return (
             <div key={index} className={styles.indicator__lineContainer}>
-              <div
-                className={`${styles.indicator__line} ${lineColor}`}
-                style={{ height: `${getLineSize(index, selectedNote ? selectedNote.freq : 0)}%` }}
-              />
+              <div className={`${styles.indicator__line} ${lineColor}`} style={{ height: `${getLineSize(index)}%` }} />
             </div>
           );
         })}
