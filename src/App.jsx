77ccf3ec413f62cdc74092a4ae73167e073d433b
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2024-12-16 21:49:14 -0300

    Add transition an App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./components/Footer/Footer.jsx";
 import NavBar from "./components/NavBar/NavBar.jsx";
@@ -7,12 +7,24 @@ import Games from "./components/Games/Games.jsx";
 
 function App() {
   const [section, setSection] = useState("tuner");
+  const [fadeOut, setFadeOut] = useState(false);
+  const [currentSection, setCurrentSection] = useState(section); // Estado para el instrumento actual
+
+  useEffect(() => {
+    setFadeOut(true);
+
+    const timeout = setTimeout(() => {
+      setCurrentSection(section);
+      setFadeOut(false);
+    }, 500);
+    return () => clearTimeout(timeout); // Limpiar el timeout si el componente se desmonta
+  }, [section]);
   return (
-    <div className="app">
+    <div className={`app ${fadeOut ? "fadeOut" : "fadeIn"}`}>
       <NavBar setSection={setSection} />
       <div className="containerSpa flex-c-c w-h-100">
-        {section === "tuner" ? <Tuner /> : null}
-        {section === "games" ? <Games /> : null}
+        {currentSection === "tuner" ? <Tuner /> : null}
+        {currentSection === "games" ? <Games /> : null}
       </div>
       <Footer />
     </div>
